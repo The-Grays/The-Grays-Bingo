b20215eca1d677f154887c3baae3578f0f65ac97
Robert King <timking@bigkinglsu.com>
2023-09-28 16:28:39 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,16 @@ import drawDeck from "./DrawingManager";
 import printDeck from "./PrintingManager";
 import generateDeck from "./BingoGenerator";
 
-const OUTPUT_DIRECTORY: string = './output';
-const timeStamp: number = Date.now();
-const fileName: string = `Bingo_${timeStamp}`;
-const filePath: string = `${OUTPUT_DIRECTORY}/${fileName}`;
+async function ProcessDeck(deck: Map<string, number[]>[]) {
+    const OUTPUT_DIRECTORY: string = './output';
+    const timeStamp: number = Date.now();
+    const fileName: string = `Bingo_${timeStamp}`;
+    const filePath: string = `${OUTPUT_DIRECTORY}/${fileName}`;
+    
+    await drawDeck(deck, OUTPUT_DIRECTORY, fileName);
+    emailDeck(players, deck, filePath);
+}
 
 const deck: Map<string, number[]>[] = generateDeck(players.length);
 printDeck(deck);
-drawDeck(deck, OUTPUT_DIRECTORY, fileName);
-emailDeck(players, deck, filePath);
+ProcessDeck(deck);
